0dc620ecc64e06a3e0e4d5f30f3803f41bacf463
User1 <User1@LAPTOP-SK1787QF>
2017-08-14 04:11:36 -0400

    Second Commit

--- src/pagePackage/LoginPageFactory.java
+++ src/pagePackage/LoginPageFactory.java
@@ -1,11 +1,5 @@
 package pagePackage;
 
-import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
--- src/testPackage/TestBrkgAccAppl.java
+++ src/testPackage/TestBrkgAccAppl.java
@@ -1,18 +1,16 @@
 package testPackage;
 
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
+
 
 import static org.junit.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+
 
 import pagePackage.LoginPageFactory;
 import pagePackage.ConfigTradePermPageFactory;
@@ -89,7 +87,6 @@ public class TestBrkgAccAppl {
 	 *2) Save and Continue
 	 */
 	
-	
 	@Test (priority=2)	
 	public void test2() {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
@@ -100,7 +97,6 @@ public class TestBrkgAccAppl {
 	 * Test Case 2: Selects All products under North America and continue
 	 */
 	
-		
 	@Test (priority=3)	
 	public void test3() {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
